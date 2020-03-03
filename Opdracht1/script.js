6790d28fc5f2cf212e335e8dcac658f45178fd9f
Cornell van der Straaten <30183@ma-web.nl>
2020-03-03 13:54:25 +0100

    Done

---
+++ Opdracht1/script.js
@@ -0,0 +1,48 @@
+let startTime 
+let endTime;
+
+const timeButton = document.getElementById('timeButton');
+const elapsedTimeH1 = document.getElementById('elapsedTime');
+
+const list = document.getElementById('previousTimesList');
+
+function startCount() {
+    startTime = new Date();
+    changeButtonOnStart();
+}
+function endCount() {
+    endTime = new Date();
+    var timeDiff = endTime - startTime; //in ms
+    // strip the ms
+    timeDiff /= 1000;
+
+    // get seconds 
+    var seconds = Math.round(timeDiff);
+    elapsedTimeH1.innerHTML = seconds + ' seconds.';
+
+    changeButtonOnStop();
+    createListItem(startTime, endTime, seconds);
+}
+
+function changeButtonOnStart() {
+    timeButton.style.backgroundColor = "red";
+    timeButton.style.Color = "black";
+    timeButton.innerHTML = "STOP";
+    timeButton.setAttribute('onclick', 'endCount()');
+}
+
+
+function changeButtonOnStop() {
+  timeButton.style.backgroundColor = "green";
+  timeButton.style.Color = "white";
+  timeButton.innerHTML = "START";
+  timeButton.setAttribute('onclick', 'startCount()');
+}
+
+function createListItem(startingDate, endingDate, timePassed){
+    const listItem = document.createElement('li');
+    listItem.className = 'listItem';
+    listItem.innerHTML = 'Start geklikt om: ' + startingDate + '. <br><br> Stop geklikt om: ' + endingDate +'. <br><br>' + 'Verstreken tijd is: ' + timePassed + '.';
+    list.prepend(listItem);
+
+}
